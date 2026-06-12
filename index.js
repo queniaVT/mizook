bc1a33dec1b71cd971d2874239bde908e15dc4f0
NotMasterG <161164873+NotMasterG@users.noreply.github.com>
2026-06-12 17:24:55 +0200

    undid bugfixing due to it causing more bugs

--- index.js
+++ index.js
@@ -221,14 +221,8 @@ client.on('messageCreate', async (message) => {
 		const tmpmsg = await message.channel.send({content: "mizook is trying their best to think..."});
 		thinkingz = true;
 
-		let content = "";
 		let llm = chooseLLM(message.content);
-		let LLMtagMatch = message.content.match(/!llm=([a-zA-Z0-9_-]+)/);
-		if (LLMtagMatch){
-			content = "<" + message.author.username + "> " + message.content.replace(LLMtagMatch[0], '').trim();
-		} else {
-			content = "<" + message.author.username + "> " + message.content;
-		}
+		content = "<" + message.author.username + "> " + message.content;
 		if (!content || content.trim().length === 0) return;
 		
 		// optional: fiwteww twiggers or smth idk
