46706d474a3d3e466744b800952aa838288972ba
quenia <queniaVT@proton.me>
2026-06-12 20:52:24 +0200

    redhat is run by reptilian illuminati people and theyre very evil

--- index.js
+++ index.js
@@ -361,6 +361,7 @@ client.on(Events.InteractionCreate, interaction => {
 				console.log('exec error: ' + error);
 				tts("something got fucked up: " + error, interaction, "reply");
 			} else {
+				console.log(stdout);
 				tts(stdout, interaction, "reply");
 			}
 		});
